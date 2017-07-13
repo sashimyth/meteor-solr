108e8116f243239e73f65a7e728fa26f17d3b352
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2017-07-13 15:49:03 +0700

    fixing template color

--- imports/ui/components/Footer/Footer.js
+++ imports/ui/components/Footer/Footer.js
@@ -8,9 +8,12 @@ const Footer = () => {
       <div className="row">
         <div className="col-md-12 text-center">
           <img className="img-responsive center" src="/images/logo.png" alt="UGM Fisipol DigiLib" />
-          <p>Copyright &copy; 2017, Faculty of Social &amp; Political Sciences, Universitas Gadjah Mada</p>
+          <h3>Fakultas Ilmu Sosial dan Ilmu Politik - Universitas Gadjah Mada</h3>
           <p>Sosio Yustisia Bulaksumur, Yogyakarta 55281</p>
           <p>Telp: (0274) 563362 | Fax: (0274) 563362 | Email: dekan_sp@ugm.ac.id</p>
+        </div>
+        <div className="footer-bot">
+          Copyright &copy; 2017, Faculty of Social &amp; Political Sciences, Universitas Gadjah Mada
         </div>
       </div>
     </footer>
